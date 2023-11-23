0b419231420ab143b4147fb618701497fffcbab8
VictorFuentesV <vhfuentesv@gmail.com>
2023-11-23 14:52:11 -0300

    Se agrega el proyecto

---
+++ proyectogrupo10/src/Data.jsx
@@ -0,0 +1,61 @@
+const DATA = [
+    {
+        id : 0,
+        title : "IPhone 11",
+        price : 230,
+        desc : "Experimenta la innovación con el iPhone 11. Captura momentos impresionantes con su cámara dual de 12 MP, disfruta de colores vibrantes en la pantalla Liquid Retina de 6.1 pulgadas y experimenta un rendimiento excepcional con el chip A13 Bionic. Con un diseño elegante y duradero, batería para todo el día y la potencia de iOS, el iPhone 11 está listo para llevarte más allá. ¡Haz clic en Comprar Ahora y descubre un nuevo nivel de tecnología móvil!",
+        img : "/assets/images/products/iphone11.png"
+    },
+    {
+        id : 1,
+        title : "IPhone 11 Pro Max",
+        price : 385,
+        desc : "Captura momentos profesionales con la triple cámara de 12 MP, disfruta de colores intensos en la pantalla Super Retina XDR de 6.5 pulgadas y experimenta un rendimiento excepcional con el chip A13 Bionic. Con un diseño elegante en acero inoxidable, batería para todo el día y la potencia de iOS, el iPhone 11 Pro Max está listo para llevar tu experiencia móvil al siguiente nivel. ¡Haz clic en Comprar Ahora y descubre lo que significa ser Pro!",
+        img : "/assets/images/products/iphone11promax.png"
+    },
+    {
+        id : 2,
+        title : "IPhone 12 Mini",
+        price : 275,
+        desc : "Experimenta lo máximo en tamaño compacto con el iPhone 12 Mini. Pantalla Super Retina, potente cámara dual, rendimiento A14 Bionic y conectividad 5G, todo en un diseño elegante de aluminio. Haz clic en Comprar Ahora y lleva la potencia del iPhone 12 Mini a tus manos.",
+        img : "/assets/images/products/iphone12mini.png"
+    },
+    {
+        id : 3,
+        title : "IPhone 12",
+        price : 285,
+        desc : "Descubre el iPhone 12, un salto adelante en tecnología. Con su pantalla Super Retina XDR de 6.1 pulgadas, potente cámara dual de 12 MP, rendimiento A14 Bionic y conectividad 5G, redefine tus expectativas. El diseño elegante y la experiencia iOS completa hacen del iPhone 12 una elección excepcional. Haz clic en Comprar Ahora y lleva la revolución a tus manos.",
+        img : "/assets/images/products/iphone12.png"
+    },
+    {
+        id : 4,
+        title : "IPhone 12 Pro",
+        price : 400,
+        desc : "Descubre el iPhone 12 Pro, donde la excelencia se encuentra con la innovación. Con una pantalla Super Retina XDR de 6.1 pulgadas, triple sistema de cámaras Pro de 12 MP, rendimiento A14 Bionic y conectividad 5G, este dispositivo eleva tu experiencia a nuevos niveles. El diseño en acero inoxidable y la versatilidad de iOS hacen del iPhone 12 Pro una elección profesional. Haz clic en Comprar Ahora y sumérgete en la excelencia tecnológica.",
+        img : "/assets/images/products/iphone12pro.png"
+    },
+    {
+        id : 5,
+        title : "IPhone 12 Pro Max",
+        price : 518,
+        desc : "Explora nuevas alturas con el iPhone 12 Pro Max. Su pantalla Super Retina XDR de 6.7 pulgadas, triple sistema de cámaras Pro de 12 MP, rendimiento A14 Bionic y conectividad 5G ofrecen una experiencia incomparable. El diseño en acero inoxidable refleja elegancia y robustez. Sumérgete en la grandeza tecnológica haciendo clic en Comprar Ahora y lleva el iPhone 12 Pro Max a tus manos.",
+        img : "/assets/images/products/iphone12promax.png"
+    },
+    {
+        id : 6,
+        title : "IPhone 14",
+        price : 899,
+        desc : "Explora el iPhone 14, donde la innovación y la elegancia convergen. Con una pantalla OLED de 6,1 pulgadas, triple sistema de cámaras de 12 Mpx, rendimiento A15 Bionic y conectividad 5G, este dispositivo lleva tu experiencia a nuevas alturas. Con su diseño elegante y resistente en acero inoxidable, junto con la versatilidad de iOS, el iPhone 14 es la elección perfecta. Haz clic en Comprar Ahora y adéntrate en la excelencia tecnológica.",
+        img : "/assets/images/products/iphone14.png"
+    },
+    {
+        id : 7,
+        title : "IPhone 13",
+        price : 799,
+        desc : "Descubre el iPhone 13, la culminación de la innovación de Apple. Con una pantalla Super Retina XDR de 6.1 pulgadas, potente sistema de doble cámara de 12 MP, rendimiento excepcional gracias al chip A15 Bionic y conectividad 5G, este dispositivo redefine tus expectativas. Su diseño elegante y resistente, combinado con la experiencia fluida de iOS, convierte al iPhone 13 en una elección incomparable. Haz clic en Comprar Ahora y sumérgete en la próxima generación de tecnología móvil.",
+        img : "/assets/images/products/iphone13.png"
+    }
+    
+]
+
+export default DATA;
